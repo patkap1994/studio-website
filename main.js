9015c6260a73c3975c4599792692a0219b7c02df
Patryk Kapkowski <patryk.kapkowski@gmail.com>
2018-07-31 14:14:10 +0200

    Ended all website with responsiveness

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 /* jshint esversion:6 */
 
+//##################################################################################################
 //Hamburger menu dropdown toggle
+//##################################################################################################
 
 let toggleNavStatus = false;
-let hamburgerMenu = document.querySelector(".hamburger-menu");
-let hamburgerMenuButton = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuButton = document.querySelector(".hamburger-icon");
 
 function toggleNav() {
     let navList = document.querySelector(".nav-list");
 
-    if(toggleNavStatus === false) {
+    if (toggleNavStatus === false) {
         navList.style.height = "100%";
 
         toggleNavStatus = true;
@@ -29,4 +31,80 @@ function toggleNav() {
 hamburgerMenu.addEventListener("click", toggleNav);
 
 
+//##############################################################################################
+//"Show more" button for showing more portfolio projects
+//##############################################################################################
 
+const showMoreButton = document.querySelector('.btn.btn-black.btn-black-invisible');
+const portfolioContainer = document.querySelector('.portfolio-container');
+
+let showFullPortfolio = false;
+
+showMoreButton.addEventListener('click', toggleFullPortfolio);
+
+function toggleFullPortfolio(e) {
+    e.preventDefault();
+
+    if (!showFullPortfolio) {
+        portfolioContainer.classList.add('show-portfolio');
+
+        showMoreButton.innerText = 'Show less';
+
+        showFullPortfolio = true;
+    } else {
+        portfolioContainer.classList.remove('show-portfolio');
+
+        showMoreButton.innerText = 'Load more';
+
+        showFullPortfolio = false;
+    }
+}
+
+
+//##############################################################################################
+//Changing quotes in quote section
+//##############################################################################################
+
+const quoteContainers = document.querySelectorAll('.quote-container');
+const dots = document.querySelectorAll('.dot');
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', changeQuote);
+}
+
+function changeQuote() {
+    clearInterval(changeQuoteContainer);
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active-dot');
+        quoteContainers[i].classList.remove('quote-container-active');
+    }
+
+    this.classList.add('active-dot');
+
+    quoteContainers[this.dataset.number - 1].classList.add('quote-container-active');
+}
+
+//##############################################################################################
+//Changing quotes in quote section with interval function
+//##############################################################################################
+let actualQuote = 1;
+
+//Change quote every 6s
+let changeQuoteContainer = setInterval(changeQuoteTiming, 6000);
+
+function changeQuoteTiming() {
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active-dot');
+        quoteContainers[i].classList.remove('quote-container-active');
+    }
+
+    dots[actualQuote].classList.add('active-dot');
+    quoteContainers[actualQuote].classList.add('quote-container-active');
+
+    //Increase the array number of actually visible quote
+    actualQuote += 1;
+
+    //Check if this is the last quote in the collection and if it is then show first quote
+    actualQuote == 4 ? actualQuote = 0 : null;
+}
